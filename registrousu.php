640db9ecd7fb1f824234f61a80f3318a5700c651
alexa07njr <160448132+alexa07njr@users.noreply.github.com>
2024-12-01 20:11:03 -0500

    Update registrousu.php

--- registrousu.php
+++ registrousu.php
@@ -1,24 +1,20 @@
 <?php
-// Incluir la conexión a la base de datos
+
 include 'conexion.php';
 
-// Verificar que los datos se envíen por POST
+
 if ($_SERVER['REQUEST_METHOD'] === 'POST') {
-    // Obtener los datos del formulario
     $nombres = $conn->real_escape_string($_POST['nombres']);
     $cedula = $conn->real_escape_string($_POST['cedula']);
     $cargo = $conn->real_escape_string($_POST['cargo']);
     $password = $conn->real_escape_string($_POST['password']);
     $rol = ($cargo === 'Supervisor') ? 1 : 2; // Asignar el rol según el cargo
 
-    // Encriptar la contraseña
     $passwordHash = password_hash($password, PASSWORD_BCRYPT);
 
-    // Preparar la consulta SQL para insertar los datos
     $sql = "INSERT INTO usuario (cedula, nombres, contrasena, rol) 
             VALUES ('$cedula', '$nombres', '$passwordHash', $rol)";
 
-    // Ejecutar la consulta y verificar si se insertaron los datos correctamente
     if ($conn->query($sql) === TRUE) {
         echo "<script>
             alert('Registro exitoso');
@@ -29,6 +25,5 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     }
 }
 
-// Cerrar la conexión
 $conn->close();
 ?>
