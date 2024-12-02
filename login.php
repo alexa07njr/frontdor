1442026b1852db2829d7a5900a9d27a983f7ae29
alexa07njr <160448132+alexa07njr@users.noreply.github.com>
2024-12-01 20:06:17 -0500

    Update login.php

--- login.php
+++ login.php
@@ -1,31 +1,26 @@
 <?php
-// Iniciar la sesión
 session_start();
 
-// Incluir la conexión a la base de datos
+
 include 'conexion.php';
 
 if ($_SERVER['REQUEST_METHOD'] === 'POST') {
-    // Obtener los datos enviados desde el formulario
     $cedula = $conn->real_escape_string($_POST['cedula']);
     $password = $_POST['password'];
 
-    // Verificar las credenciales en la tabla 'usuario'
     $sql = "SELECT * FROM usuario WHERE cedula = '$cedula'";
     $result = $conn->query($sql);
 
     if ($result->num_rows > 0) {
         $usuario = $result->fetch_assoc();
 
-        // Verificar la contraseña
+        
         if (password_verify($password, $usuario['contrasena'])) {
-            // Guardar datos en la sesión
-            $_SESSION['usuario'] = $usuario['nombres']; // Nombre del usuario
-            $_SESSION['rol'] = $usuario['rol'];         // Rol del usuario
-            $_SESSION['id_usuario'] = $usuario['id_usu']; // ID del usuario
-            $_SESSION['cedula'] = $usuario['cedula'];   // Cédula del usuario
+            $_SESSION['usuario'] = $usuario['nombres']; 
+            $_SESSION['rol'] = $usuario['rol'];       
+            $_SESSION['id_usuario'] = $usuario['id_usu']; 
+            $_SESSION['cedula'] = $usuario['cedula'];  
 
-            // Redirigir según el rol del usuario
             if ($usuario['rol'] == 1) {
                 header("Location: inicio_super.php");
             } elseif ($usuario['rol'] == 2) {
