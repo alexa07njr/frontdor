e83719423949a316d36e38ef4ede80662d2fd23d
alexa07njr <160448132+alexa07njr@users.noreply.github.com>
2024-12-01 20:00:50 -0500

    Update index.php

--- index.php
+++ index.php
@@ -8,17 +8,15 @@
     <link rel="stylesheet" href="index.css">
     <script>
         function iniciarSesion(event) {
-            event.preventDefault(); // Evita que el formulario se envíe por defecto
-            // Aquí puedes agregar la lógica para iniciar sesión
+            event.preventDefault(); 
             alert("Iniciando sesión...");
-            window.location.href = "inicio_pr.php"; // Redirige a la página de inicio
+            window.location.href = "inicio_pr.php"; 
         }
 
         function registrarse(event) {
-            event.preventDefault(); // Evita que el formulario se envíe por defecto
-            // Aquí puedes agregar la lógica para el registro
+            event.preventDefault(); 
             alert("Redirigiendo a la página de registro...");
-            window.location.href = "registro.php"; // Redirige a la página de registro
+            window.location.href = "registro.php"; 
         }
     </script>
 </head>
@@ -27,7 +25,6 @@
 
     <div class="container">
         <div class="info">
-            <!-- Agregar el logo de la empresa -->
             <img src="imagenes/Logo.png" alt="Logo de la empresa" class="logo">
             <h2>Bienvenido</h2>
             <hr>
@@ -42,17 +39,14 @@
             <h2>Inicio de sesión</h2>
             <br>
             <div class="inputs">
-                <!-- Validación del correo electrónico con dominio específico -->
                 <input type="text" name="cedula" class="box" placeholder="Cédula" required>
                 <input type="password" name="password" class="box" placeholder="Contraseña" required>
                 <a href="recuperar_contra.php">¿Olvidaste tu contraseña?</a>
 
-
-                <!-- Botón para iniciar sesión -->
                 <input type="submit" value="INICIAR SESIÓN" style="background-color: #0632ad; color: #fff; height: 30px; border-radius: 30px;">
 
                 <br>
-                <!-- Botón para registrarse -->
+
                 <input type="button" value="REGISTRARSE" style="background-color: #0632ad; color: #fff; height: 30px; border-radius: 30px;" onclick="registrarse(event)">
             </div>
         </form>
